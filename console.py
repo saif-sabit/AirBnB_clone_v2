e14fd652086f400da2deca741e4302b851c20138
saif-sabit <saifsabit@gmail.com>
2023-09-19 01:14:59 -0700

    pycode style fix

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] == '{' and pline[-1] =='}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -126,10 +126,10 @@ class HBNBCommand(cmd.Cmd):
         new_instance = HBNBCommand.classes[arg_list[0]]()
         for param in params:
             key, value = param.split("=")
-            value = value.replace("\"","")
+            value = value.replace("\"", "")
         if hasattr(new_instance, key):
             if key in HBNBCommand.types:
-                    value = HBNBCommand.types[key](value)
+                value = HBNBCommand.types[key](value)
         setattr(new_instance, key, value)
         storage.save()
         print(new_instance.id)
@@ -328,6 +328,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
