0c827428b8fb51608c6632d865c7455fc4ce299e
saif-sabit <saifsabit@gmail.com>
2023-10-19 19:26:31 +0300

    Remove -

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     """ returns c text"""
-    return f'c {text}'
+    return f'c {text.replace("_"," ")}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
