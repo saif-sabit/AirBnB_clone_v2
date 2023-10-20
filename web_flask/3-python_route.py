6bd218e1f5f6f61b88f88599bdcd349013ad7bf3
saif-sabit <saifsabit@gmail.com>
2023-10-20 23:45:38 +0300

    Task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
 """
-start Flask application
+Start Flask application
+web application specifications:
+    Listening on 0.0.0.0, port 5000
+    Routes:
+        /: display 'Hello HBNB!'
+        /hbnb: display 'HBNB'
+        /c/<text>: display c is value of text
 """
 
-from flask import Flask
+from flask import Flask, escape
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ returns HBNB"""
-    return 'HBNB'
+    """
+    Route display "HBNB"
+    """
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     """ returns c text"""
-    return f'c {text.replace("_"," ")}'
+    return f"C {escape(text.replace('_',' '))}"
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def pyhton_route(text = "is cool"):
     """ returns python text"""
-    return f'Python  {text.replace("_"," ")}'
+    return f'Python  {escape(text.replace("_"," "))}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
