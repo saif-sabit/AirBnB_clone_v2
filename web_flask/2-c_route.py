1ee067081449965f87f7cd66628a103f5cce6706
saif-sabit <saifsabit@gmail.com>
2023-10-20 23:26:15 +0300

    doc

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """
-start Flask application
+Start Flask application
+web application specifications:
+    Listening on 0.0.0.0, port 5000
+    Routes:
+        /: display 'Hello HBNB!'
+        /hbnb: display 'HBNB'
+        /c/<text>: display c is value of text
 """
 
 from flask import Flask
@@ -10,17 +16,26 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ returns HBNB"""
-    return 'HBNB'
+    """
+    Route display "HBNB"
+    """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     """ returns c text"""
     return f'c {text.replace("_"," ")}'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
