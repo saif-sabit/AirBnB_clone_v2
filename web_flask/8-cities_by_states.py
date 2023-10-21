15b5d00929bc8e93bb25497ab54110671de1b71d
saif-sabit <saifsabit@gmail.com>
2023-10-21 13:01:28 +0300

    Cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+start Flask application
+"""
+
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list_route():
+    """returns states list"""
+    states = sorted(storage.all("State").values())
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
